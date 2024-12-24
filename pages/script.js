1434d83b688854ce9f250929d59b9200da8486a4
Ak <pavani.cdp@gmail.com>
2024-12-23 19:24:14 -0800

    Create script.js

---
+++ pages/script.js
@@ -0,0 +1,21 @@
+// To print "Hello, World!" to the console:
+console.log("Hello, World!");
+
+// To declare a variable and assign a value:
+let message = "Hello, World!";
+console.log(message);
+
+// To create a function:
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greet("Alice"); 
+
+// To use an if statement:
+let age = 18;
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are not an adult.");
+}
